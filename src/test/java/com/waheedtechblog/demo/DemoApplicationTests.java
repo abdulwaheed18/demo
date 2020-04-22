975cbe8c7317966e674eb811ac8d34333f8d447b
ABdul Waheed <abdulwaheed18@gmail.com>
2020-04-22 16:22:03 +0530

    Added junit test cases

--- src/test/java/com/waheedtechblog/demo/DemoApplicationTests.java
+++ src/test/java/com/waheedtechblog/demo/DemoApplicationTests.java
@@ -1,13 +1,20 @@
 package com.waheedtechblog.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	private MessageController messageController;
+
 	@Test
 	void contextLoads() {
+		assertThat(messageController).isNotNull();
 	}
 
 }
---
+++ src/test/java/com/waheedtechblog/demo/MessageControllerTest.java
@@ -0,0 +1,25 @@
+package com.waheedtechblog.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class MessageControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void greetingShouldReturnDefaultMessage() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/name/Waheed",
+                String.class)).contains("Hi Waheed .");
+    }
+}
